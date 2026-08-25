9c11eef3d183c395fe5e43635ff851b929de21a6
Rhauan Rafael Martins Medeiros <rhauanrafael2505@gmail.com>
2026-08-25 02:26:40 +0000

    Prepara backend para deploy

--- server/index.js
+++ server/index.js
@@ -50,6 +50,8 @@ ${mensagem}
   }
 });
 
-app.listen(3000, () => {
-  console.log("Servidor rodando na porta 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
 });
